e20a0e47797fd3cee7b7fcad3ed380479e210dae
Sebastian Raubitzek <50580205+Raubkatz@users.noreply.github.com>
2023-04-19 09:30:19 +0200

    Add files via upload

--- VQC_RandomizedSearch_final.py
+++ VQC_RandomizedSearch_final.py
@@ -2,6 +2,9 @@ import numpy as np
 from sklearn.datasets import load_iris, load_breast_cancer, load_wine
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelBinarizer
+from sklearn.datasets import fetch_openml
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.base import BaseEstimator, ClassifierMixin
 from qiskit import Aer
 from qiskit.utils import QuantumInstance
@@ -10,6 +13,7 @@ from qiskit.algorithms.optimizers import SPSA
 from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes, PauliFeatureMap, ZFeatureMap, EfficientSU2, TwoLocal
 from qiskit.algorithms.optimizers import COBYLA, SPSA, ADAM, L_BFGS_B, TNC, NFT
 from datetime import datetime
+from copy import deepcopy as dc
 import sys
 
 def build_feature_map(feature_map_class, feature_dimension):
@@ -56,9 +60,9 @@ class VQCWrapper(BaseEstimator, ClassifierMixin):
 n_random_picks = 1
 
 # Choose data set
-data_nr = 0 #0: iris, 1: breast cancer, 2:
+data_nr = 0 #0: iris, 1: breast cancer, 2: wine data set, 3: adult data set aka census income
 
-# Load and preprocess the Iris dataset
+# Load and preprocess the Iris dataset, all features of these datasets are conitnuous no additional preprocessing required
 if data_nr == 0:
     data_sk = load_iris()
     data_name = "iris"
@@ -68,7 +72,9 @@ elif data_nr == 1:
 elif data_nr == 2:
     data_sk = load_wine()
     data_name = "wine"
-
+elif data_nr == 3:
+    data_sk = fetch_openml(name='adult', version=1, as_frame=True)
+    data_name = "adult"
 else:
     print('No valid data choice, exiting...')
     sys.exit()
@@ -76,14 +82,25 @@ else:
 X = data_sk.data
 y = data_sk.target
 
-# Split the dataset
-scaler = MinMaxScaler().fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+if data_nr == 3: #we need to do additional preprocessing for the adult data set
 
-# Scale the features
-#scaler = MinMaxScaler().fit(X_train)
-#X_train = scaler.transform(X_train)
-#X_test = scaler.transform(X_test)
+    # Identify categorical and continuous features
+    cat_features = X.select_dtypes(include=['object']).columns
+    cont_features = X.select_dtypes(include=[np.number]).columns
+
+    # Create a ColumnTransformer instance with OneHotEncoder for categorical features and MinMaxScaler for continuous features
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', OneHotEncoder(), cat_features),
+            ('cont', MinMaxScaler(), cont_features)
+        ])
+
+    # Preprocess the features
+    X = dc(preprocessor.fit_transform(X))
+else:
+    scaler = MinMaxScaler().fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Binarize labels
 label_binarizer = LabelBinarizer()
